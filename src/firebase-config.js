44f90e60e4b375415c6a0ac23e87ff14eaafe7cc
niranjanblank <niranjanshah474@gmail.com>
2021-12-22 13:19:59 +0545

    env added

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -9,15 +9,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyARs73hUvVU5fbLoSOzCHq41KLA-8_Y1L4",
-  authDomain: "react-firestore-a0cd3.firebaseapp.com",
-  projectId: "react-firestore-a0cd3",
-  storageBucket: "react-firestore-a0cd3.appspot.com",
-  messagingSenderId: "775791956150",
-  appId: "1:775791956150:web:c1dffb227b15d065671617",
-  measurementId: "G-3JZ5KWNDQQ"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
-
+console.log('env',process.env.apiKey_own)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
