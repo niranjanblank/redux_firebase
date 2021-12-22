653044a42964f484165e3aef137e50c1d544ae01
niranjanblank <niranjanshah474@gmail.com>
2021-12-22 13:28:43 +0545

    changed firebase information

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyARs73hUvVU5fbLoSOzCHq41KLA-8_Y1L4",
-  authDomain: "react-firestore-a0cd3.firebaseapp.com",
-  projectId: "react-firestore-a0cd3",
-  storageBucket: "react-firestore-a0cd3.appspot.com",
-  messagingSenderId: "775791956150",
-  appId: "1:775791956150:web:c1dffb227b15d065671617",
-  measurementId: "G-3JZ5KWNDQQ"
+  apiKey: "AIzaSyCuNNnh98nYHxgL4fyXqOYSXWCt4FWJ4hQ",
+  authDomain: "test-5949b.firebaseapp.com",
+  projectId: "test-5949b",
+  storageBucket: "test-5949b.appspot.com",
+  messagingSenderId: "370786180266",
+  appId: "1:370786180266:web:e881d095313801588d44d3",
+  measurementId: "G-V25EC0LPD0"
 };
 // console.log('env',process.env.apiKey_own)
 // Initialize Firebase
